e3d63728aa209a3dbc4a8888122893f5f5a1ac52
junyeong.jo <whwnsdud2024@naver.com>
2023-07-12 09:58:17 +0900

    응답 - 정적 리소스, 뷰 템플릿

---
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,33 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-07-12
+ */
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1(Model model) {
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello1");
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello2");
+        return "response/hello";
+    }
+
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello3");
+    }
+}
